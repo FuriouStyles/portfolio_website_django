e5263f9b617e1c67c9ab098834b4202b62c98738
Stephen <sjplaint@gmail.com>
2020-02-17 12:54:36 -0500

    routing for notes fixed, needs styling and update/delete views & routes

--- notes/models.py
+++ notes/models.py
@@ -7,7 +7,6 @@ from django.urls import reverse
 
 
 class Notes(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
     title = models.CharField(max_length=100)
     colab = models.CharField(max_length=255, blank=True, default='')
     youtube = models.CharField(max_length=255, blank=True, default='')
--- notes/urls.py
+++ notes/urls.py
@@ -4,6 +4,6 @@ from .views import ClassesListView, ClassesDetailView, ClassesCreateView
 
 urlpatterns = [
     path('', ClassesListView.as_view(), name='class-notes'),
-    path('class/<int:pk>/', ClassesDetailView.as_view(), name='note-detail'),
-    path('class/note/new/', ClassesCreateView.as_view(), name='note-create')
+    path('<int:pk>/', ClassesDetailView.as_view(), name='note-detail'),
+    path('new/', ClassesCreateView.as_view(), name='note-create')
 ]
