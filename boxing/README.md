34837f4163ec6bc026c3695c90b85de8776c5354
Stephen <sjplaint@gmail.com>
2020-04-22 12:49:35 -0400

    removed duplicate files from previous merge

--- boxing/README.md
+++
@@ -1,2 +0,0 @@
-# portfolio_website_django
-This is the repository for my portfolio website, powered by Django and Bootstrap.
--- boxing/blog/templatetags/sidebar.py
+++
@@ -1,9 +0,0 @@
-from django import template
-
-register = template.Library()
-
-
-@register.inclusion_tag("sidebar.html")
-def show_sidebar(Post):
-    posts = Post.choice_set.all()
-    return {"posts": posts}
